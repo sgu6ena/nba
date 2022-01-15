8503dfd111d63561589ce6ed8435919bb4247208
sgu6ena <sgu6ena@gmail.com>
2022-01-15 17:35:21 +0200

    edit card - add img + type

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -3,13 +3,31 @@ import "./styles.scss";
 
 interface ICardProps {
   title: string;
+  place?: string;
+  subtitle: string;
+  img: string;
+  type: "command" | "player";
 }
 
-const Card: React.FunctionComponent<ICardProps> = ({ title }) => {
+const Card: React.FunctionComponent<ICardProps> = ({
+  title,
+  subtitle,
+  place,
+  img,
+  type,
+}) => {
   return (
     <div className="card">
-      <div className="card_image"></div>
-      <div className="card_description"> {title}</div>
+      <div className={type}  >
+        <img src={img} alt={title}  />
+      </div>
+      <div className="card_description">
+        <div className="title">
+          {title}
+          <span className="red"> {place}</span>
+        </div>
+        <div className="subtitle">{subtitle}</div>
+      </div>
     </div>
   );
 };
