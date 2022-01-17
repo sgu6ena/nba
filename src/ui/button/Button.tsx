7ab74ec7d094bf6d84c2ec33c56cf2ff70371dc4
sgu6ena <sgu6ena@gmail.com>
2022-01-17 22:39:54 +0200

    fix button styles

--- src/ui/button/Button.tsx
+++ src/ui/button/Button.tsx
@@ -7,7 +7,7 @@ export interface IButtonProps {
   }
   
 const Button: React.FC<IButtonProps> = ({disabled, secondary,children}) =>{
-    return <button disabled={disabled} className={secondary?"secondary":''}>{children}</button>
+    return <button disabled={disabled} className={secondary?"secondary button":'button'}>{children}</button>
 } 
 
 export default Button;
