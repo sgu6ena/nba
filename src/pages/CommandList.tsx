e104334b755fea5827bfc3fbdeec050e2aa52f38
sgu6ena <issar@admin.idknet.com>
2022-01-18 11:30:28 +0200

    init page commandlist

---
+++ src/pages/CommandList.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface ICommandListProps {
+}
+
+export function CommandList (props: ICommandListProps) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
