c7d89b71e922c7633696571a57cec0f173117aea
sgu6ena <issar@admin.idknet.com>
2022-01-18 11:31:22 +0200

    fix s

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Route, Routes,BrowserRouter  } from "react-router-dom";
+import { Command } from "./pages/Command";
+import { CommandList } from "./pages/CommandList";
 import { PageNotFound } from "./pages/PageNotFound";
 import Test from "./pages/Test";
 
@@ -10,7 +12,9 @@ function App() {
     <div className="App"> 
        <BrowserRouter>    <Routes>
         <Route path="/" element={<Test />} />
-        <Route path="about" element={<PageNotFound />} />
+        <Route path="/commands"  element={<CommandList/>}/>
+        <Route path="/command/:id" element={<Command />}/>
+        <Route path="*" element={<PageNotFound />} />
       </Routes></BrowserRouter>
 
     </div>
