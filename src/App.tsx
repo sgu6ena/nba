0d7cc01c318e9e466278234fdd1aa4f3ea6678e6
sgu6ena <issar@admin.idknet.com>
2022-01-19 17:53:13 +0200

    grid page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import React from "react";
-import { Route, Routes,BrowserRouter  } from "react-router-dom";
+import { Route, Routes, BrowserRouter, Link } from "react-router-dom";
+import Header from "./components/header/Header";
+import Sidebar from "./components/sidebar/Sidebar";
 import { Command } from "./pages/Command";
 import { CommandList } from "./pages/CommandList";
 import { PageNotFound } from "./pages/PageNotFound";
 import Test from "./pages/Test";
 
-
 function App() {
   return (
-
-    <div className="App"> 
-       <BrowserRouter>    <Routes>
-        <Route path="/" element={<Test />} />
-        <Route path="/commands"  element={<CommandList/>}/>
-        <Route path="/command/:id" element={<Command />}/>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes></BrowserRouter>
-
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Sidebar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Test />} />
+            <Route path="/commands" element={<CommandList />} />
+            <Route path="/command/:id" element={<Command />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
