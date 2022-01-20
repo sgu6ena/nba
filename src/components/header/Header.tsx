856aa369e704c689f29f9253858aabd8e96b6077
sgu6ena <sgu6ena@gmail.com>
2022-01-20 23:32:52 +0200

    refactor: add link on logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,19 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 import Logo from "../../assets/images/logo.png";
-import UserAvatar from "../user-avatar/UserAvatar";
+import Avatar from "../user-avatar/UserAvatar";
 
 import "./styles.scss";
 
-export interface IHeaderProps {
- 
-}
+export interface IHeaderProps {}
 
 const Header: React.FC<IHeaderProps> = () => {
   return (
-    <div className="header" >
-     <img src={Logo} alt="logo" height="48" />
-     <UserAvatar name="John Smith"/>
+    <div className="header">
+      <Link to="/">
+        <img src={Logo} alt="logo" height="48" />
+      </Link>
+      <Avatar name="John Smith" avatarUrl="https://cdn.vox-cdn.com/thumbor/N6-QGX2FaDUgPW3-RRqoM3dfpkQ=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19979927/jomi_avatar_nickleodeon_ringer.jpg" />
     </div>
   );
 };
