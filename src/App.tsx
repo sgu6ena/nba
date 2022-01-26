8f59b3686fceceaad3fbcaaf75a998c1ebfddef7
sgu6ena <issar@admin.idknet.com>
2022-01-26 09:57:37 +0200

    test api post, get - login token

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Provider } from "react-redux";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { setupStore } from "./store/store";
 
-
 import { Command } from "./pages/Command";
 import { CommandList } from "./pages/CommandList";
 import Login from "./pages/Login";
@@ -14,32 +13,39 @@ import Register from "./pages/Register";
 import Test from "./pages/Test";
 import ApiService from "./services/api";
 
-
 const store = setupStore();
 
 function App() {
-
   const api = new ApiService();
   api.getPing().then(console.log);
-  api.getVersion().then(console.log)
+  api.getVersion().then(console.log);
+  api
+    .postLogin("string", "string")
+    .then((data) => console.log("успешная авторизация, токен - ", data.token))
+    .catch(() => console.log("ошибка авторизации"));
+
+    api
+    .postLogin("string", "stdssring")
+    .then((data) => console.log(data.token))
+    .catch(() => console.log("ошибка авторизации"));
 
   return (
     <div className="App">
       <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Test />} />
-          <Route path="/commands" element={<CommandList />} />
-          <Route path="/command/:id" element={<Command />} />
-          <Route path="/commands/test" element={<Command />} />
-          <Route path="/players" element={<PlayerList />} />
-          <Route path="/players/:id" element={<Player />} />
-          <Route path="/players/test" element={<Player />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Test />} />
+            <Route path="/commands" element={<CommandList />} />
+            <Route path="/command/:id" element={<Command />} />
+            <Route path="/commands/test" element={<Command />} />
+            <Route path="/players" element={<PlayerList />} />
+            <Route path="/players/:id" element={<Player />} />
+            <Route path="/players/test" element={<Player />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
       </Provider>
     </div>
   );
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,27 +1,68 @@
+interface IHeaders {
+  "Content-Type": string;
+  Accept: string;
+}
 
+type IMethod = "GET" | "POST" | "PUT" | "DELETE";
 
 export default class ApiService {
-    _apiBaseUrl = `//dev.trainee.dex-it.ru`;
+  _apiBaseUrl = `//dev.trainee.dex-it.ru/`;
 
-    getResource = async (url:string) =>{
-        const res = await  fetch(`${this._apiBaseUrl}${url}`);
+  headers: any = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  };
 
-        if (!res.ok) {
-            throw new Error (`Could not fetch resource: ${url} - status: ${res.status}`);
-        }
+  getResource = async (url: string) => {
+    const res = await fetch(`${this._apiBaseUrl}${url}`, {
+      headers: this.headers,
+      method: "GET",
+    });
 
-        return await res.json();
+    if (!res.ok) {
+      throw new Error(
+        `Could not fetch resource: ${url} - status: ${res.status}`
+      );
     }
 
-    getPing = async () => {
-        return await this.getResource(`/api/echo/ping`);
-    }
-    getVersion = async () => {
-        return await this.getResource(`/api/echo/version`);
+    return await res.json();
+  };
+
+  postResource = async (url: string, data: any = {}) => {
+    const res = await fetch(`${this._apiBaseUrl}${url}`, {
+      headers: this.headers,
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+      throw new Error(
+        `Could not fetch resource: ${url} - status: ${res.status}`
+      );
     }
 
-    getCar = async (id: string | undefined | number) => {
-        return await this.getResource(`/cars/${id}`);
-    }
+    return await res.json();
+  };
+
+  getPing = async () => {
+    return await this.getResource(`api/echo/ping`);
+  };
+  getVersion = async () => {
+    return await this.getResource(`api/echo/version`);
+  };
+
+  postLogin = async (login: string, pass: string) => {
+    return await this.postResource(`api/Auth/SignIn`, {
+      login: login,
+      password: pass,
+    });
+  };
+
+  postRegister = async (login: string, pass: string) => {
+    return await this.postResource(`api/Auth/SignIn`, {
+      login: login,
+      password: pass,
+    });
+  };
 
 }
