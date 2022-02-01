93a7ed91ac679fc800f86cf36d8b854f6a84a07e
sgu6ena <sgu6ena@gmail.com>
2022-02-01 23:57:49 +0200

    мобильное отображение менб

--- src/components/menu-item/MenuItem.tsx
+++ src/components/menu-item/MenuItem.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { NavLink } from "react-router-dom";
-// import "./styles.scss";
 import styled from "styled-components";
 import * as vars from "../../assets/variables/variables";
 
@@ -10,40 +9,54 @@ interface IMenuItemProps {
 }
 
 const StyledMenuItem = styled.div`
-
-cursor: pointer;
-transition: all 0.2s;
-padding: 16px;
-color: ${vars.$lightGrey};
-display: flex;
-justify-content: center;
-flex-direction: column;
-align-items: center;
-
-a {
-  color: inherit;
-  margin-top: 4px;
-  text-align: center;
-  text-decoration: none;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 12px;
-  line-height: 150%;
-}
-
-&:hover {
+  cursor: pointer;
   transition: all 0.2s;
-  color: ${vars.$lightestRed};
-}
-a.active {
-  color: ${vars.$red};
-}
-svg{
+  padding: 16px;
+  color: ${vars.$lightGrey};
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  @media screen and (max-width: ${vars.$md}) { padding: 16px 5px;}
+  a {
+    color: inherit;
+    margin-top: 4px;
+    text-align: center;
+    text-decoration: none;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 150%;
+
+    @media screen and (max-width: ${vars.$md}) {
+  
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      flex-direction: row;
+      svg {
+        height: 36px;
+        width: 36px;
+        margin-right: 20px;
+      }
+    }
+  }
+
+  &:hover {
+    transition: all 0.2s;
+    color: ${vars.$lightestRed};
+  }
+  a.active {
+    color: ${vars.$red};
+  }
+  svg {
     height: 24px;
     width: 24px;
-}
-
-`
+  }
+  @media screen and (max-width: ${vars.$md}) {
+    align-items: start;
+  }
+`;
 
 const MenuItem: React.FunctionComponent<IMenuItemProps> = ({
   to,
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -8,38 +8,72 @@ import { ReactComponent as GroupPerson } from "../../assets/icons/group-person.s
 import { ReactComponent as Person } from "../../assets/icons/person.svg";
 import { ReactComponent as Input } from "../../assets/icons/input.svg";
 import { RouteNames } from "../../router";
+import Avatar from "../user-avatar/UserAvatar";
+import { useAuth } from "../../hooks/user-auth";
 
 interface ISidebarProps {}
 
 const Nav = styled.nav`
+  z-index: 20;
   padding: 1.2em;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  transition: all 0.5s;
-  @media screen and (max-width: ${vars.$md}) {
-    display: none;
-    transition: all 0.5s;
+  transition: all 3s;
+  .menu-wrapper {
+    width: 100%;
   }
+  .UserAvatar {
+    display: none;
+  }
+  @media screen and (max-width: ${vars.$md}) {
+    background-color: white;
+    position: fixed;
+    top: 81px;
+    left: -200px;
+    display: none;
+    transition: all 3s;
+    width: 200px;
+    height: calc(100% - 80px);
+    &.active {
+      transition: all 3s;
+      left: 0px;
+      display: flex;
+    }
+    .UserAvatar {
+      padding: 10px 4px;
+      width: 100%;
+      display: flex;
+      justify-content: flex-end;
+      border-bottom: 1px solid ${vars.$lightestGrey};
+      svg{ 
+        width: 40px;
+        height: 40px;
+      }
+    }
+  }
+`;
+const Sidebar: React.FunctionComponent<ISidebarProps> = () => {
+  const user = useAuth();
+  return (
+    <Nav className="sidebar ">
+      <div className="menu-wrapper">
+        <Avatar name={user.name} avatarUrl={user.avatarUrl} type="left" />
 
-`
-const Sidebar: React.FunctionComponent<ISidebarProps> = () => (
-  <Nav>
-    <div>
-      <MenuItem to={RouteNames.TEAMS} title="Teams">
-        <GroupPerson />
-      </MenuItem>
-      <MenuItem to={RouteNames.PLAYERS} title="Players">
-        <Person />
-      </MenuItem>
-    </div>
-   
+        <MenuItem to={RouteNames.TEAMS} title="Teams">
+          <GroupPerson />
+        </MenuItem>
+        <MenuItem to={RouteNames.PLAYERS} title="Players">
+          <Person />
+        </MenuItem>
+      </div>
 
-    <MenuItem to="/singout" title="Exit">
+      <MenuItem to="/singout" title="Exit">
         <Input />
       </MenuItem>
-  </Nav>
-);
+    </Nav>
+  );
+};
 
 export default Sidebar;
